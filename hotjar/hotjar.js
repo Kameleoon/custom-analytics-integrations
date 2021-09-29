0099177c66fbf1ad89828ccd13f927cefcb307bd
michael <mmunhurrun@kameleoon.com>
2021-09-29 13:58:46 +0200

    Update Hotjar custom integration / Adding session recording

--- hotjar/hotjar.js
+++ hotjar/hotjar.js
@@ -7,7 +7,11 @@ const id = experimentId ? experimentId : personalizationId;
 const processHotjar = function () {
     let tag_name = `${id}-${name}-${variationID}-${variationName}`;
     tag_name = tag_name.replaceAll(" ", "").substring(0, 250);
+    
     window.hj('event', tag_name);
+
+    // if you don’t use Hotjar’s session recording feature, you can remove the line
+    window.hj('tagRecording', [tag_name]);
 };
 
 Kameleoon.API.Core.runWhenConditionTrue(function () {
