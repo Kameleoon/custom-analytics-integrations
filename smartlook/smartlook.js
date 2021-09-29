9cf661d4eb514b872e4b7a03e0a4aa9f87b9c502
michael <mmunhurrun@kameleoon.com>
2021-09-29 13:51:26 +0200

    Add Smartlook custom integration

---
+++ smartlook/smartlook.js
@@ -0,0 +1,17 @@
+// Define the name of the current experiment or personalization
+const name = experimentName ? experimentName : personalizationName;
+
+// Define the id of the current experiment or personalization
+const id = experimentId ? experimentId : personalizationId;
+
+const processSmartlook = function () {
+    const properties = {
+        "test/variation": `${id} - ${name} - ${variationID} - ${variationName}`,
+    };
+
+    window.smartlook('track', "Kameleoon campaign", properties);
+};
+
+Kameleoon.API.Core.runWhenConditionTrue(function () {
+    return window.smartlook != null;
+}, processSmartlook, 150);
