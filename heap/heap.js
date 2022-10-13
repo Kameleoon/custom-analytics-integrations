ba8cde3ad65bc95d0d7f292e83be9ed01f35ba22
Alexey Kalyuzhnyi <akalyuzhnyi@kameleoon.com>
2022-10-13 10:45:28 +0400

    update heap integration

--- heap/heap.js
+++ heap/heap.js
@@ -1,14 +1,22 @@
 // Define the name of the current experiment or personalization
 const name = experimentName ? experimentName : personalizationName;
+const id = experimentId ? experimentId : personalizationId;
 
-const processHeap = function() {
+const processHeap = function () {
     heap.track("Kameleoon", {
         name: name,
-        variationName: variationName
+        variationName: variationName,
     });
-    heap.addUserProperties({ experimentName: name, variationName: variationName });
+    heap.addUserProperties({
+        ["experimentName-" + id]: name,
+        ["variationName-" + variationID]: variationName,
+    });
 };
 
-Kameleoon.API.Core.runWhenConditionTrue(function() {
-    return window.heap != null;
-}, processHeap, 150);
+Kameleoon.API.Core.runWhenConditionTrue(
+    function () {
+        return window.heap != null;
+    },
+    processHeap,
+    150
+);
