c501b3fb0df9ffbe55e28e674c44430853435699
Yassin Hassan <yhassan@kameleoon.com>
2024-09-17 16:00:11 +0200

    Adding LogRocket

---
+++ logrocket/logrocket.js
@@ -0,0 +1,27 @@
+// Define the name of the current experiment or personalization
+const name = experimentName ? experimentName : personalizationName;
+
+// Define the id of the current experiment or personalization
+const id = experimentID ? experimentID : personalizationID;
+
+// Define the event type
+const eventType = experimentID ? "Experiment" : "Personalization";
+
+const processLogRocket = function () {
+  LogRocket.track("Kameleoon", {
+    event: eventType,
+    campaign_id: id,
+    campaign_name: name,
+    variation_id: variationID,
+    variation_name: variationName,
+  });
+};
+
+//Pass campaign information into LogRocket
+Kameleoon.API.Core.runWhenConditionTrue(
+  function () {
+    return window.LogRocket != null;
+  },
+  processLogRocket,
+  150
+);
