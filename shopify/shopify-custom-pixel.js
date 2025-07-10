713546b1679fe84a180b0aac1df18d95dd1c450f
Isabelle <iung@kameleoon.com>
2025-07-10 10:05:12 +0200

    update shopify replacing kameleoon.js to engine.js

--- shopify/shopify-custom-pixel.js
+++ shopify/shopify-custom-pixel.js
@@ -16,7 +16,7 @@ analytics.subscribe("page_viewed", (event) => {
       script.type = "text/javascript";
 
       // Set the src attribute to the Kameleoon JavaScript URL
-      script.src = "//" + siteCode + ".kameleoon.io/kameleoon.js";
+      script.src = "//" + siteCode + ".kameleoon.io/engine.js";
 
       // Set async attribute to true
       script.async = true;
